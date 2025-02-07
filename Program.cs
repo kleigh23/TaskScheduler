8ef337d49cf04e715d461f2f22504601390a9262
Kelley Robertson <hkleigh42@gmail.com>
2025-02-06 17:35:52 -0700

    finished readme file and added comments to program file

--- Program.cs
+++ Program.cs
@@ -4,38 +4,48 @@ using System.Timers;
 
 class TaskScheduler
 {
+    // This is the time.
     private static System.Timers.Timer timer = null!;
+    // This is the list that is remembering all the tasks.
     private static List<ScheduledTask> tasks = new List<ScheduledTask>();
 
     static void Main()
     {
-        timer = new System.Timers.Timer(60000); // Check every minute
+        timer = new System.Timers.Timer(60000); // Check every minute.
         timer.Elapsed += CheckTasks;
         timer.Start();
 
+        // This is what  is being sent to the command line/user and is the beginning of the loop.
         Console.WriteLine("Task Scheduler Started. Type 'exit' to quit.");
         while (true)
-        {
+        {   
+            // This is the prompt that is given at the beginning or after task has been entered.
             Console.Write("Enter task description (or 'exit' to quit): ");
+
+            // This is checking to read what the user has entered and to check if they typed exit if not what to do next
             string? description = Console.ReadLine();
             if (description?.ToLower() == "exit") break;
 
             if (!string.IsNullOrWhiteSpace(description))
             {
+                // This shows them how to enter their reminder for their task
                 Console.Write("Enter task date and time (yyyy-MM-dd HH:mm): ");
                 if (DateTime.TryParse(Console.ReadLine(), out DateTime taskTime))
                 {
+                    // Telling the user its been added
                     tasks.Add(new ScheduledTask(description, taskTime));
                     Console.WriteLine("Task added successfully!");
                 }
                 else
                 {
+                    // Telling the user that what they entered wasn't right 
                     Console.WriteLine("Invalid date format. Please try again.");
                 }
             }
         }
     }
 
+    // This is the function that checks to see if the time has come and if so to send the reminder to the user/commandline
     private static void CheckTasks(object? sender, ElapsedEventArgs e)
     {
         DateTime now = DateTime.Now;
@@ -50,6 +60,7 @@ class TaskScheduler
     }
 }
 
+// This is creating a class that holds the data the user entered.
 class ScheduledTask
 {
     public string Description { get; }
--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Overview
 
-{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
 I created this program to test my knowledge I gained on the programing language C#. I also thought it would be a good program that was more difficult but within my new knowledge. I also enjoyed trying to implement different things from the libraries that are available in the C# language. I also could see how learning this could help me better prepare for other programs in the future. 
 
-{Provide a description of the software that you wrote to demonstrate the C# language.}
 Its a Task Scheduler and reminder, so it prompts the user for a task name and then time to be reminded. Then when the time/date comes around it will display the task in the command line. It uses different libraries that are available in the C# language, and with this language it makes it so then if oyu wanted to you could use GUI intergration using Windows, Forms, or WPF.
 
-{Describe your purpose for writing this software.}
 I wanted to create this software to test my knowledge not only on the C# language but also my programing knowledge. I have done something simular to this but at the same time not the same. 
 
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/7KEocQ3Jag4)
 
 # Development Environment
 
@@ -33,8 +29,6 @@ I wanted to create this software to test my knowledge not only on the C# languag
 - [Microsoft: Learn C#](https://dotnet.microsoft.com/en-us/learn/csharp)
 
 # Future Work
-
-{Make a list of things that you need to fix, improve, and add in the future.}
 
 - Save tasks to a file or database
 - Send notifications via email
